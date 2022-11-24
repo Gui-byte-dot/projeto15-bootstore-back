22b6b60faa01736fef34f7edcc91ccac06c3cdd8
duuda2011@gmail.com <elissonalvessilva@MacBook-Pro-de-Elisson.local>
2022-11-24 11:33:56 -0300

    refactor: reconectanto mongoDB

--- src/database/db.js
+++ src/database/db.js
@@ -2,7 +2,9 @@ import {MongoClient} from 'mongodb';
 import dotenv from 'dotenv';
 dotenv.config();
 
+
 const mongoClient = new MongoClient(process.env.MONGO_URI);
+const db = mongoClient.db(process.env.DATABASE_NAME);
 
 try{
     await mongoClient.connect();
@@ -10,6 +12,6 @@ try{
 }catch(err){
     console.log(err);
 }
-const db = mongoClient.db("bookstore");
 export const usersCollection = db.collection("users");
 export const sessionsCollection = db.collection("sessions");
+export const productCollection = db.collection("product");
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { signIn, signUp } from "../controllers/auth.controllers.js";
+import { signIn, signUp} from "../controllers/auth.controllers.js";
 import { userSchemaValidation, signInBodyValidation} from "../middlewares/authValidation.middleware.js";
 const router = Router();
 
 router.post("/sign-up", userSchemaValidation, signUp)
 router.post("/sign-in", signInBodyValidation, signIn)
-router.post("/products", )
+
 
 export default router;              
